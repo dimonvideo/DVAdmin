e6b22c439b519d3f5d9d47d0766206870942b5d1
dimonvideo-beep <dimonvideo@gmail.com>
2020-06-22 18:13:00 +0300

    init

--- dvadmin/src/main/java/dv/dimonvideo/dvadmin/MainActivity.java
+++ dvadmin/src/main/java/dv/dimonvideo/dvadmin/MainActivity.java
@@ -2,6 +2,7 @@ package dv.dimonvideo.dvadmin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
@@ -9,22 +10,47 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
     MyRecyclerViewAdapter adapter;
-    int countUploader = 0;
-    int countVuploader = 0;
-    int countMuzon = 0;
-    int countUsernews = 0;
-    int countGallery = 0;
-    int countForum = 0;
+    String countUploader;
+    String countVuploader;
+    String countMuzon;
+    String countUsernews;
+    String countGallery;
+    String countForum;
+    String countUrl = "https://api.dimonvideo.ru/smart/dvadminapi.php?op=18";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +60,27 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(true);
 
-        ArrayList<String> Names = new ArrayList<>();
+        set_adapter();
+
+
+
+
+
+    }
+
+
+    // получение данных
+    private void set_adapter(){
+
+        final ArrayList<String> count = new ArrayList<>();
+        count.add(countUploader);
+        count.add(countVuploader);
+        count.add(countMuzon);
+        count.add(countUsernews);
+        count.add(countGallery);
+        count.add(countForum);
+
+        final ArrayList<String> Names = new ArrayList<>();
         Names.add(getString(R.string.uploader));
         Names.add(getString(R.string.vuploader));
         Names.add(getString(R.string.muzon));
@@ -42,23 +88,63 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         Names.add(getString(R.string.gallery));
         Names.add(getString(R.string.forum));
 
-        ArrayList<String> count = new ArrayList<>();
-        count.add(String.valueOf(countUploader));
-        count.add(String.valueOf(countVuploader));
-        count.add(String.valueOf(countMuzon));
-        count.add(String.valueOf(countUsernews));
-        count.add(String.valueOf(countGallery));
-        count.add(String.valueOf(countForum));
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, countUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
 
-        RecyclerView recyclerView = findViewById(R.id.rv);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapter(this, Names, count);
-        adapter.setClickListener(this);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        dividerItemDecoration.setDrawable(Objects.requireNonNull(ContextCompat.getDrawable(MainActivity.this, R.drawable.divider)));
-        recyclerView.addItemDecoration(dividerItemDecoration);
-        recyclerView.setAdapter(adapter);
+                            countUploader = jsonObject.getString("uploader");
+                            countVuploader = jsonObject.getString("vuploader");
+                            countMuzon = jsonObject.getString("muzon");
+                            countUsernews = jsonObject.getString("usernews");
+                            countGallery = jsonObject.getString("gallery");
+                            countForum = jsonObject.getString("forum");
+
+                            count.clear();
+                            count.add(countUploader);
+                            count.add(countVuploader);
+                            count.add(countMuzon);
+                            count.add(countUsernews);
+                            count.add(countGallery);
+                            count.add(countForum);
+
+                            RecyclerView recyclerView = findViewById(R.id.rv);
+                            recyclerView.setHasFixedSize(true);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+                            dividerItemDecoration.setDrawable(Objects.requireNonNull(ContextCompat.getDrawable(MainActivity.this, R.drawable.divider)));
+                            recyclerView.addItemDecoration(dividerItemDecoration);
+                            adapter = new MyRecyclerViewAdapter(getApplicationContext(), Names, count);
+                            adapter.setClickListener(MainActivity.this);
+                            recyclerView.setAdapter(adapter);
+
+
+
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), getString(R.string.error_network_timeout), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_network_timeout), Toast.LENGTH_LONG).show();
+                } else if (error instanceof AuthFailureError) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_network_timeout), Toast.LENGTH_LONG).show();
+                } else if (error instanceof ServerError) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_server), Toast.LENGTH_LONG).show();
+                } else if (error instanceof NetworkError) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_network), Toast.LENGTH_LONG).show();
+                } else if (error instanceof ParseError) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_server), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        queue.add(stringRequest);
 
     }
 
@@ -89,5 +175,33 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
 
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK) {
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyLongPress(keycode, event);
+    }
 
 }
