27fa054a5b3f03d740c3e6d4d1b915176f5e1095
admin <dimon@dimonvideo.ru>
2024-09-06 11:06:56 +0300

    4.1 API change

--- dvadmin/src/DVAdmin_Samsung/java/dv/dimonvideo/dvadmin/util/Analytics.java
+++ dvadmin/src/DVAdmin_Samsung/java/dv/dimonvideo/dvadmin/util/Analytics.java
@@ -2,12 +2,18 @@ package dv.dimonvideo.dvadmin.util;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import dv.dimonvideo.dvadmin.Config;
+import dv.dimonvideo.dvadmin.MainActivity;
 
 public class Analytics {
 
@@ -15,21 +21,18 @@ public class Analytics {
 
         final boolean is_notify = AppController.getInstance().is_notify();
 
-        try {
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setApplicationId("1:50549051988:android:a46a6e539a88fde4e7d3c1") // Required for Analytics.
-                    .setProjectId("dvadmin-5a6d2") // Required for Firebase Installations.
-                    .build();
-            FirebaseApp.initializeApp(context, options, "DVAdmin");
-
-            if (is_notify) {
-
-                Log.v(Config.TAG, "!!!! ====== NOTICE ======== !!!! ");
-
-                FirebaseMessaging.getInstance().subscribeToTopic("DVAdmin");
-            } else FirebaseMessaging.getInstance().unsubscribeFromTopic("DVAdmin");
-        } catch (Throwable ignored) {
-        }
+        FirebaseMessaging.getInstance().subscribeToTopic("weather")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Subscribed";
+                        if (!task.isSuccessful()) {
+                            msg = "Subscribe failed";
+                        }
+                        Log.d(Config.TAG, msg);
+                        Toast.makeText(context.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
