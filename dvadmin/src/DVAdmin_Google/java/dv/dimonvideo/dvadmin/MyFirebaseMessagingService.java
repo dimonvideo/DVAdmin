8f79de553db4e9daf70f5617e1712b7251ee1cda
admin <dimon@dimonvideo.ru>
2024-09-06 12:23:42 +0300

    4.1 API change

--- dvadmin/src/DVAdmin_Google/java/dv/dimonvideo/dvadmin/MyFirebaseMessagingService.java
+++ dvadmin/src/DVAdmin_Google/java/dv/dimonvideo/dvadmin/MyFirebaseMessagingService.java
@@ -1,14 +1,27 @@
 package dv.dimonvideo.dvadmin;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.List;
+import java.util.Objects;
 
 import dv.dimonvideo.dvadmin.util.WidgetProvider;
 
@@ -29,11 +42,59 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Log.v(Config.TAG, "!!!! ====== NOTICE ======== !!!! ");
 
+        if (remoteMessage.getNotification() != null) {
+            sendNotification(remoteMessage.getNotification().getBody());
+        }
     }
 
     @Override
     public void onNewToken(@NonNull String token) {
 
+    }
+
+    private void sendNotification(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // channel for notifications
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+            if (notificationManager != null) {
+                List<NotificationChannel> channelList = notificationManager.getNotificationChannels();
+
+                for (int i = 0; channelList != null && i < channelList.size(); i++)
+                    notificationManager.deleteNotificationChannel(channelList.get(i).getId());
+            }
+
+            String name = getString(R.string.app_name);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(getString(R.string.app_name), name, importance);
+
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent p_intent;
+        p_intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(p_intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.app_name))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentTitle("DVAdmin")
+                .setContentText(text)
+                .setContentIntent(pendingIntent);
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        notificationManager.notify(0, builder.build());
     }
 
 }
